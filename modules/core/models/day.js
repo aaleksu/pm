801567cd3e4b41420f17400c0a0300030ab3f6de
Alex <a.aleksu@gmail.com>
2016-01-07 23:10:50 +0300

    init commit

---
+++ modules/core/models/day.js
@@ -0,0 +1,13 @@
+angular.module('core').factory('Day', ['$rootScope', function($rootScope){
+    function Day(){
+        this.date = null;
+    }
+
+    Day.prototype = {
+        set_date: function(date){
+            this.date = date;
+        }
+    };
+
+    return Day;
+}]);
